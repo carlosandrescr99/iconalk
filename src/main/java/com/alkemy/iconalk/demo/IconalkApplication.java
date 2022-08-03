f754c5551d7def83d221c97d1c9bf7eeaeac63e6
Andres.C <carlosandrescr99@gmail.com>
2022-08-02 23:32:10 -0500

    initial commit

---
+++ src/main/java/com/alkemy/iconalk/demo/IconalkApplication.java
@@ -0,0 +1,14 @@
+package com.alkemy.iconalk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IconalkApplication.class, args);
+		System.out.println("Api Run...");
+	}
+
+}
---
+++ src/test/java/com/alkemy/iconalk/iconalk/IconalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.iconalk.iconalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
